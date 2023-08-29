bcf5cff7e53cb4d44e4d3904df44df08f301c01d
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-30 00:54:26 +0900

    ✨ データを展開して表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { PostType } from "./types/posts"
+
 async function fetchAllPosts() {
   const res = await fetch('http://localhost:3000/api/blog', {
     cache: 'no-store'
@@ -11,13 +13,19 @@ export default async function Home() {
   const posts = await fetchAllPosts()
 
   return (
-    <main className='flex items-center justify-center w-screen h-screen'>
-      <h1>Next.js Prisma Supabase Blog</h1>
-      {posts.map((post: any) => (
-        <div>
-          {post.title}
-        </div>
-      ))}
+    <main className='flex flex-col items-center justify-center gap-4 w-screen h-screen'>
+      <div>
+        <h1 className="text-xl">Next.js Prisma Supabase Blog</h1>
+      </div>
+      <div className="flex flex-col gap-4 w-1/3">
+        {posts.map((post: PostType) => (
+          <div className="p-8 shadow-md">
+            <div>
+              {post.title}
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
