369f8e8e934aad74e62570f7b9bd5f8da5429014
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-01 12:33:54 +0900

    ⚡️ 編集前のデータをフォームに表示

--- src/app/blog/edit/[id]/page.tsx
+++ src/app/blog/edit/[id]/page.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Link from 'next/link'
+import { useEffect, useRef } from 'react'
+import { Toaster, toast } from 'react-hot-toast'
 
 const getBlogById = async (id: number) => {
   const res = await fetch(`http://localhost:3000/api/blog/${id}`)
@@ -6,20 +10,39 @@ const getBlogById = async (id: number) => {
   return data.post
 }
 
-const EditBlog = () => {
+const EditBlog = ({ params }: { params: { id: number } }) => {
+  const titleRef = useRef<HTMLInputElement | null>(null)
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null)
+
+  useEffect(() => {
+    getBlogById(params.id)
+      .then((data) => {
+        if (titleRef.current && descriptionRef.current) {
+          titleRef.current.value = data.title
+          descriptionRef.current.value = data.description
+        }
+      })
+      .catch((err) => {
+        toast.error('An error has occurred.', { id: '1' })
+      })
+  }, [])
+
   return (
     <main className='flex flex-col items-center justify-center gap-4 w-screen h-screen'>
+      <Toaster />
       <div>
         <h1 className="text-xl">Edit Post</h1>
       </div>
       <div className="w-1/3 p-8 shadow-md">
         <form className="flex flex-col gap-4">
           <input
+            ref={titleRef}
             type="text"
             placeholder="Title"
             className="px-4 py-2 border rounded-md"
           />
           <textarea
+            ref={descriptionRef}
             placeholder="Description"
             rows={3}
             className="px-4 py-2 border rounded-md"
